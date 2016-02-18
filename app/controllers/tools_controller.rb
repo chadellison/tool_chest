8ace0788575fde5df14ba89719574cbfd57c989d
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-18 11:28:28 -0700

    fleshed out REST functionality

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -10,4 +10,38 @@ class ToolsController < ApplicationController
   def new
     @tool = Tool.new
   end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    if @tool.update(tool_params)
+      redirect_to tool_path(@tool.id)
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @tool = Tool.find(params[:id])
+    @tool.destroy
+    redirect_to tools_path
+  end
+
+  private
+
+  def tool_params #gives us security by only allowing the permitted values to be entered.
+    params.require(:tool).permit(:name, :quantity, :price)
+  end
 end
