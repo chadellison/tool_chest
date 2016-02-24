6479057b4cc3270d4d05f17625d7d8463ae00a64
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-24 14:38:44 -0700

    add admin feature test

--- test/features/admin_can_create_update_read_and_delete_all_tools_test.rb
+++ test/features/admin_can_create_update_read_and_delete_all_tools_test.rb
@@ -1,15 +1,16 @@
-# require 'test_helper'
-#
-# class AdminCanCreateUpdateReadDeleteAllToolsTest < ActionDispatch::IntegrationTest
-#   test "admin can create update read and delete any tools" do
-#     admin = User.create(username: "admin", password: "password", role: 1)
-#
-#     ApplicationController.any_instance.stubs(:current_user).returns(admin)
-#
-#     visit admin_tools_path
-#
-#     assert page.has_content?("All Tools")
-#
-#
-#   end
-# end
+require 'test_helper'
+
+class AdminCanCreateUpdateReadDeleteAllToolsTest < ActionDispatch::IntegrationTest
+  test "admin can create update read and delete any tools" do
+    admin = User.create(username: "admin", password: "password", role: 1)
+    
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_tools_path
+
+    assert page.has_content?("All Tools")
+
+
+
+  end
+end
